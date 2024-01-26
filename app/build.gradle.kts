6855d2112455110ad433f327007d3be185f0f24a
makich <ivanovdd06@gmail.com>
2024-01-26 14:57:56 +0300

    day1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ android {
 }
 
 dependencies {
+    implementation("androidx.compose.ui:ui-util:1.5.1")
     implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("androidx.core:core-ktx:1.10.1")
@@ -62,6 +63,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("com.google.firebase:firebase-messaging-ktx:23.4.0")
+    implementation("androidx.navigation:navigation-compose:2.7.6")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/mtusi_timetable/MainActivity.kt
+++ app/src/main/java/com/example/mtusi_timetable/MainActivity.kt
@@ -14,33 +14,57 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.util.lerp
 import androidx.core.app.ActivityCompat
 import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
 import androidx.core.content.ContextCompat
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.mtusi_timetable.ui.theme.Mtusi_timetableTheme
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.RemoteMessage
+import kotlin.math.absoluteValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting(this)
+            TimeTable(this)
         }
     }
 }
@@ -56,35 +80,149 @@ fun checkNotificationPermission(context: Context){
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun Greeting(context: Context) {
+    //        checkNotificationPermission(context)
+    FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+        if (!task.isSuccessful) {
+            Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+            return@OnCompleteListener
+        }
+
+        // Get new FCM registration token
+        val token = task.result
+        Log.w(TAG, token.toString())
+    })
 
     Box(modifier = Modifier
         .fillMaxSize()
         .background(Color.Gray)){
-        checkNotificationPermission(context)
-        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
-            if (!task.isSuccessful) {
-                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
-                return@OnCompleteListener
-            }
 
-            // Get new FCM registration token
-            val token = task.result
-            Log.w(TAG, token.toString())
-        })
+    val pagerState = rememberPagerState(pageCount = {3})
 
-    val pageState = rememberPagerState(pageCount = {3})
-
-    HorizontalPager(state = pageState, modifier = Modifier.align(Alignment.Center)) { page ->
+    HorizontalPager(state = pagerState, modifier = Modifier.align(Alignment.Center)) { page ->
             when (page) {
-                0 -> Text(text = "Page 1")
-                1 -> Text(text = "Page 2")
-                2 -> Text(text = "Page 3")
+                0 -> Column(modifier = Modifier
+                    .fillMaxWidth()
+                    .graphicsLayer {
+                        val pageOffset = (
+                                (pagerState.currentPage - page) + pagerState
+                                    .currentPageOffsetFraction
+                                ).absoluteValue
+
+                        alpha = lerp(
+                            start = 0.5f,
+                            stop = 1f,
+                            fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                        )
+                    }) {
+                    Text(text = "Page 1", modifier = Modifier.align(Alignment.CenterHorizontally), textAlign = TextAlign.Center)
+                    Image(painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription = "", modifier = Modifier.align(Alignment.CenterHorizontally))
+                }
+                1 -> Column(modifier = Modifier
+                    .fillMaxWidth()
+                    .graphicsLayer {
+                        val pageOffset = (
+                                (pagerState.currentPage - page) + pagerState
+                                    .currentPageOffsetFraction
+                                ).absoluteValue
+
+                        alpha = lerp(
+                            start = 0.5f,
+                            stop = 1f,
+                            fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                        )
+                    }) {
+                    Text(text = "Page 2", modifier = Modifier.align(Alignment.CenterHorizontally), textAlign = TextAlign.Center)
+                    Image(painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription = "", modifier = Modifier.align(Alignment.CenterHorizontally))
+                }
+                2 -> Column(modifier = Modifier
+                    .fillMaxWidth()
+                    .graphicsLayer {
+                        val pageOffset = (
+                                (pagerState.currentPage - page) + pagerState
+                                    .currentPageOffsetFraction
+                                ).absoluteValue
+
+                        alpha = lerp(
+                            start = 0.5f,
+                            stop = 1f,
+                            fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                        )
+                    }) {
+                    Text(text = "Page 3", modifier = Modifier.align(Alignment.CenterHorizontally), textAlign = TextAlign.Center)
+                    Image(painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription = "", modifier = Modifier.align(Alignment.CenterHorizontally))
+                }
+
             }
         }
     }
 }
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SelectGroup(context: Context){
+    Box(modifier = Modifier.background(Color.Gray).fillMaxSize()) {
+        val groupList = listOf("group1", "group2", "group3")
+
+        Column(
+            modifier = Modifier.align(Alignment.Center)
+        ) {
+            Text(text = "Select Group")
+            var expanded by remember { mutableStateOf(false) }
+            var selectedIndex by remember { mutableIntStateOf(0) }
+
+            ExposedDropdownMenuBox(
+                expanded = expanded,
+                onExpandedChange = {
+                    expanded = !expanded
+                }
+            ) {
+                TextField(
+                    value = groupList[selectedIndex],
+                    onValueChange = {},
+                    readOnly = true,
+                    trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                    modifier = Modifier.menuAnchor()
+                )
+                ExposedDropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+                    groupList.forEachIndexed { index, s ->
+                        DropdownMenuItem(
+                            onClick = { selectedIndex = index; expanded = false },
+                            text = { Text(text = s) })
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TimeTable(context: Context) {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.Gray)) {
+        Text(text = "TimeTable")
+        val classesList = listOf("class1", "class2", "class3")
+
+        LazyColumn(modifier = Modifier.align(Alignment.Center)) {
+            items(classesList) {
+                Text(text = it)
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "HomeScreen") {
+        composable("HomeScreen") { Greeting(context = LocalContext.current) }
+    }
+}
+
+
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
